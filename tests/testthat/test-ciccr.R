c6f4a06a514779d092e7f5a47b99ca80fcbdd4bf
Sokbae Lee <sl3841@columbia.edu>
2021-02-21 21:45:47 +0000

    More tests added

--- tests/testthat/test-ciccr.R
+++ tests/testthat/test-ciccr.R
@@ -2,7 +2,7 @@ context("Testing ciccr")
 
 # code should work whether x has one variable or more
 
-test_that("The default option for avg_RR_logit is 'control'", {
+test_that("Case 1 with a scalar x: The default option for avg_RR_logit is 'control'", {
 
 y = ACS_CC$topincome
 t = ACS_CC$baplus
@@ -12,6 +12,65 @@ results_default = avg_RR_logit(y, t, x)
 results_control = avg_RR_logit(y, t, x, 'control')
 
 expect_equal( results_default$est, results_control$est)
+
+})
+
+test_that("Case 2 with x and x^2: The default option for avg_RR_logit is 'control'", {
+
+  y = ACS_CC$topincome
+  t = ACS_CC$baplus
+  x = ACS_CC$age
+  x = cbind(x,x^2)
+
+  results_default = avg_RR_logit(y, t, x)
+  results_control = avg_RR_logit(y, t, x, 'control')
+
+  expect_equal( results_default$est, results_control$est)
+
+})
+
+test_that("The results for avg_RR_logit should be different between 'case' and 'control'", {
+
+  y = ACS_CC$topincome
+  t = ACS_CC$baplus
+  x = ACS_CC$age
+
+  results_case = avg_RR_logit(y, t, x, 'case')
+  results_control = avg_RR_logit(y, t, x, 'control')
+
+  expect_false( results_case$est == results_control$est)
+
+})
+
+test_that("There should be an error other than 'case' and 'control'", {
+
+  y = ACS_CC$topincome
+  t = ACS_CC$baplus
+  x = ACS_CC$age
+
+  expect_error(avg_RR_logit(y, t, x, 'ctrl'))
+
+})
+
+test_that("Each element of 'y' must be either 0 or 1.", {
+
+  y = ACS_CC$topincome
+  t = ACS_CC$baplus
+  x = ACS_CC$age
+  y[1] = 2
+
+  expect_error(avg_RR_logit(y, t, x))
+
+})
+
+test_that("Each element of 't' must be either 0 or 1.", {
+
+  y = ACS_CC$topincome
+  t = ACS_CC$baplus
+  x = ACS_CC$age
+  t[1] = 2
+
+  expect_error(avg_RR_logit(y, t, x))
 
 })
 
