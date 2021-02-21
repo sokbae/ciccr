364e206ba3abf2fb86192808d142a0308dbb1fd7
Sokbae Lee <sl3841@columbia.edu>
2021-02-21 21:11:07 +0000

    test updated

--- tests/testthat/test-ciccr.R
+++ tests/testthat/test-ciccr.R
@@ -14,3 +14,16 @@ results_control = avg_RR_logit(y, t, x, 'control')
 expect_equal( results_default$est, results_control$est)
 
 })
+
+test_that("The default sampling option for avg_A_logit is 'cc'", {
+
+  y = ACS_CC$topincome
+  t = ACS_CC$baplus
+  x = ACS_CC$age
+
+  results_default = avg_AR_logit(y, t, x)
+  results_cc = avg_AR_logit(y, t, x, sampling = 'cc')
+
+  expect_equal( results_default$est, results_cc$est)
+
+})
